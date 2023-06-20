a22b25a61688108045b329187d5f2f5e1453510a
Christian Armstrong <christianarmstrong@Christians-MBP.lan>
2023-06-20 16:33:30 -0600

    added quil loading gif

--- src/lib/layouts/Loading1Layout.svelte
+++ src/lib/layouts/Loading1Layout.svelte
@@ -1,3 +1,15 @@
 <div class="w-screen h-screen flex flex-col gap-4 items-center justify-center">
+	<div class="text-center flex items-center">
+		<h2 class="text-xl font-medium mb-6">Generating Cover Letter...</h2>
+		<div style="flex-shrink: 0; margin-top: -2em;">
+			<p>
+				<img
+					src="https://media.giphy.com/media/Znn3Lv7p30HZv6WLOU/giphy.gif"
+					alt="Loading GIF"
+					style="max-width: 1em; height: auto;"
+				/>
+			</p>
+		</div>
+	</div>
 	<slot />
 </div>
--- src/lib/views/Loading1.svelte
+++ src/lib/views/Loading1.svelte
@@ -1,3 +1,1 @@
-<div class="text-center">
-	<h2 class="text-xl font-medium mb-6">Generating Cover Letter...</h2>
-</div>
+
