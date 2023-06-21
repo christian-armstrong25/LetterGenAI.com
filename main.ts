b72d2597d5e14193e49e7057f608dc4c6ce0d447
Christian Armstrong <christianarmstrong@Christians-MBP.lan>
2023-06-20 22:27:50 -0600

    testing github workflow for auto deploying 3

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import "./app.css";
-import App from "/Users/christianarmstrong/Desktop/CS/side-projects/Start/App.svelte";
+import App from "./App.svelte";
 
 const app = new App({
 	target: document.getElementById("app"),
